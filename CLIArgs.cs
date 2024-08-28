aa36f3b87cec916e0644d63ae30f91844645c190
ProfessorLagg <futurecake2999@gmail.com>
2024-08-28 15:58:58 +0200

    Add project files.

---
+++ CLIArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaggWoL {
+		public sealed class CLIArgs {
+				public PhysicalAddress MAC { get; private set; }
+
+				public CLIArgs(IList<string> args) {
+
+				}
+		}
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿namespace LaggWoL {
+		internal class Program {
+				static void Main(string[] args) {
+						CLIArgs cli_args = new CLIArgs(args);
+				}
+		}
+}
